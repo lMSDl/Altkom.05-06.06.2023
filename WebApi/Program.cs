702ed4c8fe113177b239d5af5aa95a03c0adada1
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-05 10:48:34 +0200

    TraceId

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,10 @@
 using Bogus;
+using Microsoft.AspNetCore.Diagnostics;
 using Models;
 using Services.Bogus;
 using Services.Bogus.Fakers;
 using Services.Interfaces;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,19 @@ builder.Services.AddTransient<BaseFaker<User>, UserFaker>();
 
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+if (Activity.Current == null)
+{
+    Console.WriteLine($"{context.TraceIdentifier}");
+}
+else
+{
+    Console.WriteLine($"00-{Activity.Current.TraceId}-{Activity.Current.SpanId}-00");
+}
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 
